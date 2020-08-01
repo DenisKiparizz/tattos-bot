c845aa26fb8465da4f829fe4984f41b04e561305
Denis Solodkiy <d.salodki@godeltech.com>
2020-08-01 11:56:31 +0300

    Initials commit

---
+++ src/main/java/com/tatto/bot/TattoBotApplication.java
@@ -0,0 +1,12 @@
+package com.tatto.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TattoBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TattoBotApplication.class, args);
+    }
+}
---
+++ src/test/java/com/tatto/bot/TattoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatto.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TattoBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
